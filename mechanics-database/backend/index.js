859325aa3bcbad909292956cd3c89e9774e25dbe
Min Jung Lee <mjlee3w@tamu.edu>
2021-07-28 10:14:01 -0500

    fixed harper connection

--- mechanics-database/backend/index.js
+++ mechanics-database/backend/index.js
@@ -21,72 +21,72 @@ app.use(express.json());
 
 app.use(cors());
 
-//POST: Create employees and add them to the database
-app.post('/add-employee', (req, res) => {
-    const { employee_id, employee_name, employee_password, job_title} = req.body;
-    db('Employee')
-        .insert({
-            employee_id: employee_id,
-            employee_name: employee_name,
-            employee_password: employee_password,
-            job_title: job_title,
-        })
-        .then(() => {
-            console.log('Employee Added');
-            return res.json({ msg: 'Employee Added' });
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
+// //POST: Create employees and add them to the database
+// app.post('/add-employee', (req, res) => {
+//     const { employee_id, employee_name, employee_password, job_title} = req.body;
+//     db('Employee')
+//         .insert({
+//             employee_id: employee_id,
+//             employee_name: employee_name,
+//             employee_password: employee_password,
+//             job_title: job_title,
+//         })
+//         .then(() => {
+//             console.log('Employee Added');
+//             return res.json({ msg: 'Employee Added' });
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         });
+// });
 
-//GET: Read all employees
-app.get('/', (req, res) => {
-    db.select('*')
-        .from('employee')
-        .then((data) => {
-            console.log(data);
-            res.json(data);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
+// //GET: Read all employees
+// app.get('/', (req, res) => {
+//     db.select('*')
+//         .from('employee')
+//         .then((data) => {
+//             console.log(data);
+//             res.json(data);
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         });
+// });
 
-// PUT: Update employee by employee_id from the database
-app.put('/update-employee', (req, res) => {
-    db('employee')
-        .where('employee_id', '=', 1)
-        .update({ employee_name: 'Divia Joseph' })
-        .then(() => {
-            console.log('Employee Name Updated');
-            return res.json({ msg: 'Employee Name Updated' });
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
+// // PUT: Update employee by employee_id from the database
+// app.put('/update-employee', (req, res) => {
+//     db('employee')
+//         .where('employee_id', '=', 1)
+//         .update({ employee_name: 'Divia Joseph' })
+//         .then(() => {
+//             console.log('Employee Name Updated');
+//             return res.json({ msg: 'Employee Name Updated' });
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         });
+// });
 
-// DELETE: Delete movie by movieId from the database
-app.delete('/delete-employee', (req, res) => {
-    const employeeID = req.body;
-    const employeeIdToDelete = String(employeeId.employee_id);
-    console.log(employeeIdToDelete);
-    db('employee')
-        .where('employee_id', '=', employeeIdToDelete)
-        .del()
-        .then(() => {
-            console.log('Employee Deleted');
-            return res.json({ msg: 'Employee Deleted' });
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
+// // DELETE: Delete movie by movieId from the database
+// app.delete('/delete-employee', (req, res) => {
+//     const employeeID = req.body;
+//     const employeeIdToDelete = String(employeeId.employee_id);
+//     console.log(employeeIdToDelete);
+//     db('employee')
+//         .where('employee_id', '=', employeeIdToDelete)
+//         .del()
+//         .then(() => {
+//             console.log('Employee Deleted');
+//             return res.json({ msg: 'Employee Deleted' });
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         });
+// });
 
 // HarperDB Database routes
 
-app.get('/online/harperdb', (req, res) => {
+app.get('/', (req, res) => {
     const data = { operation: 'sql', sql: 'SELECT * FROM Mechanics.Employee' };
 
     const config = {

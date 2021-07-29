01afe34bc9ea525bfae9d631a8387450030e474f
David Acosta <David@Davids-Mac.local>
2021-07-29 11:52:42 -0500

    Added some routes and some basic views

---
+++ mechanics-database/frontend/src/Components/AddEmployee.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link, Route, useRouteMatch } from "react-router-dom";
+
+function AddEmployee() {
+    <div className="form-container">
+        Hello!
+        </div>
+}
+
+export default AddEmployee;
---
+++ mechanics-database/frontend/src/Components/index.js
@@ -0,0 +1,6 @@
+export { default as Home } from "./Home";
+export { default as Employees } from "./Employees";
+export { default as Navigation } from "./Navigation";
+export { default as AddEmployee } from "./AddEmployee";
+export { default as Vehicles } from "./Vehicles";
+export { default as Parts } from "./Parts";

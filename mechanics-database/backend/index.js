af4392f26c799575d7fa32d2b07d61fab39c4c4c
Divia Joseph <diviajoseph1@tamu.edu>
2021-07-30 10:11:23 -0500

    Added routes for employee and salary.

--- mechanics-database/backend/index.js
+++ mechanics-database/backend/index.js
@@ -21,71 +21,50 @@ app.use(express.json());
 
 app.use(cors());
 
-// //POST: Create employees and add them to the database
-// app.post('/add-employee', (req, res) => {
-//     const { employee_id, employee_name, employee_password, job_title} = req.body;
-//     db('Employee')
-//         .insert({
-//             employee_id: employee_id,
-//             employee_name: employee_name,
-//             employee_password: employee_password,
-//             job_title: job_title,
-//         })
-//         .then(() => {
-//             console.log('Employee Added');
-//             return res.json({ msg: 'Employee Added' });
-//         })
-//         .catch((err) => {
-//             console.log(err);
-//         });
-// });
-
-// //GET: Read all employees
-// app.get('/', (req, res) => {
-//     db.select('*')
-//         .from('employee')
-//         .then((data) => {
-//             console.log(data);
-//             res.json(data);
-//         })
-//         .catch((err) => {
-//             console.log(err);
-//         });
-// });
-
-// // PUT: Update employee by employee_id from the database
-// app.put('/update-employee', (req, res) => {
-//     db('employee')
-//         .where('employee_id', '=', 1)
-//         .update({ employee_name: 'Divia Joseph' })
-//         .then(() => {
-//             console.log('Employee Name Updated');
-//             return res.json({ msg: 'Employee Name Updated' });
-//         })
-//         .catch((err) => {
-//             console.log(err);
-//         });
-// });
-
-// // DELETE: Delete movie by movieId from the database
-// app.delete('/delete-employee', (req, res) => {
-//     const employeeID = req.body;
-//     const employeeIdToDelete = String(employeeId.employee_id);
-//     console.log(employeeIdToDelete);
-//     db('employee')
-//         .where('employee_id', '=', employeeIdToDelete)
-//         .del()
-//         .then(() => {
-//             console.log('Employee Deleted');
-//             return res.json({ msg: 'Employee Deleted' });
-//         })
-//         .catch((err) => {
-//             console.log(err);
-//         });
-// });
-
 // HarperDB Database routes
 
+// POST: Create employee and add them to the database
+app.post('/add-employee', (req, res) => {
+    const { employee_id, employee_name, employee_password, job_title } = req.body;
+    console.log(req.body);
+
+    const data = {
+        operation: 'insert',
+        schema: 'Mechanics',
+        table: 'Employee',
+        records: [
+            {
+              employee_id: employee_id,
+              employee_name: employee_name,
+              employee_password: employee_password,
+              job_title: job_title
+            },
+        ],
+    };
+
+    const config = {
+        method: 'post',
+        url: process.env.HARPERDB_URL,
+        headers: {
+            Authorization: `Basic ${process.env.HARPERDB_AUTH}`,
+            'Content-Type': 'application/json',
+        },
+        data: data,
+    };
+
+    axios(config)
+        .then((response) => {
+            const data = response.data;
+            console.log('Employee Added');
+            res.json(data);
+            //return res.redirect('http://localhost:3000')
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+});
+
+//GET: Read all employees from the database
 app.get('/', (req, res) => {
     const data = { operation: 'sql', sql: 'SELECT * FROM Mechanics.Employee' };
 
@@ -109,6 +88,235 @@ app.get('/', (req, res) => {
             console.log(error);
         });
 });
+
+//GET: Read an employee by employee_ID from the DATABASE
+app.get('/:employee_id', (req, res) => {
+    const employee_id = req.params.employee_id;
+    console.log(employee_id);
+
+    const data = { operation: 'sql', sql: `SELECT * FROM Mechanics.Employee WHERE id = ${employee_id}` };
+
+    const config = {
+        method: 'post',
+        url: process.env.HARPERDB_URL,
+        headers: {
+            Authorization: `Basic ${process.env.HARPERDB_AUTH}`,
+            'Content-Type': 'application/json',
+        },
+        data: data,
+    };
+
+    axios(config)
+        .then((response) => {
+            const data = response.data;
+            console.log(data);
+            res.json(data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+});
+
+// PUT: Update employee by employee_id from the database
+app.put('/update-employee', (req, res) => {
+    const employee_id = req.body.employee_id;
+    console.log(employee_id);
+
+    const data = { operation: 'sql', sql: `UPDATE Mechanics.Employee SET employee_name = 'Nick Oberg' WHERE id = ${employee_id}` };
+
+    const config = {
+        method: 'post',
+        url: process.env.HARPERDB_URL,
+        headers: {
+            Authorization: `Basic ${process.env.HARPERDB_AUTH}`,
+            'Content-Type': 'application/json',
+        },
+        data: data,
+    };
+
+    axios(config)
+        .then((response) => {
+            res.send({ msg: 'Employee Updated' });
+            console.log('Employee Updated');
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+});
+
+//DELETE: Delete employee by employee_id from database
+app.delete('/delete-employee', (req, res) => {
+    const employee_id = req.body.employee_id;
+    console.log(employee_id);
+
+    const data = { operation: 'sql', sql: `DELETE FROM Mechanics.Employee WHERE id = ${employee_id}` };
+
+    const config = {
+        method: 'post',
+        url: process.env.HARPERDB_URL,
+        headers: {
+            Authorization: `Basic ${process.env.HARPERDB_AUTH}`,
+            'Content-Type': 'application/json',
+        },
+        data: data,
+    };
+
+    axios(config)
+        .then((response) => {
+            res.send({ msg: 'Employee Deleted' });
+            console.log('Employee Deleted');
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+});
+
+// POST: Create salary and add to the database
+app.post('/add-salary', (req, res) => {
+    const { job_title, wage } = req.body;
+    console.log(req.body);
+
+    const data = {
+        operation: 'insert',
+        schema: 'Mechanics',
+        table: 'Salary',
+        records: [
+            {
+              job_title: job_title,
+              wage: wage,
+            },
+        ],
+    };
+
+    const config = {
+        method: 'post',
+        url: process.env.HARPERDB_URL,
+        headers: {
+            Authorization: `Basic ${process.env.HARPERDB_AUTH}`,
+            'Content-Type': 'application/json',
+        },
+        data: data,
+    };
+
+    axios(config)
+        .then((response) => {
+            const data = response.data;
+            console.log(data);
+            res.json(data);
+            //return res.redirect('http://localhost:3000')
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+});
+
+//GET: Read all salaries from the database
+app.get('/', (req, res) => {
+    const data = { operation: 'sql', sql: 'SELECT * FROM Mechanics.Salary' };
+
+    const config = {
+        method: 'post',
+        url: process.env.HARPERDB_URL,
+        headers: {
+            Authorization: `Basic ${process.env.HARPERDB_AUTH}`,
+            'Content-Type': 'application/json',
+        },
+        data: data,
+    };
+
+    axios(config)
+        .then((response) => {
+            const data = response.data;
+            console.log(data);
+            res.json(data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+});
+
+//GET: Read an salary by job_title from the DATABASE
+app.get('/:job_title', (req, res) => {
+    const job_title = req.params.job_title;
+    console.log(job_title);
+
+    const data = { operation: 'sql', sql: `SELECT * FROM Mechanics.Salary WHERE id = ${job_title}` };
+
+    const config = {
+        method: 'post',
+        url: process.env.HARPERDB_URL,
+        headers: {
+            Authorization: `Basic ${process.env.HARPERDB_AUTH}`,
+            'Content-Type': 'application/json',
+        },
+        data: data,
+    };
+
+    axios(config)
+        .then((response) => {
+            const data = response.data;
+            console.log(data);
+            res.json(data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+});
+
+// PUT: Update salary by job_title from the database
+app.put('/update-salary', (req, res) => {
+    const job_title = req.body.job_title;
+    console.log(job_title);
+
+    const data = { operation: 'sql', sql: `UPDATE Mechanics.Salary SET wage = '700000' WHERE job_title = ${job_title}` };
+
+    const config = {
+        method: 'post',
+        url: process.env.HARPERDB_URL,
+        headers: {
+            Authorization: `Basic ${process.env.HARPERDB_AUTH}`,
+            'Content-Type': 'application/json',
+        },
+        data: data,
+    };
+
+    axios(config)
+        .then((response) => {
+            res.send({ msg: 'Salary Updated' });
+            console.log('Salary Updated');
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+});
+
+//DELETE: Delete Salary by job_title from database
+app.delete('/delete-salary', (req, res) => {
+    const job_title = req.body.job_title;
+    console.log(job_title);
+
+    const data = { operation: 'sql', sql: `DELETE FROM Mechanics.Salary WHERE job_title = ${job_title}` };
+
+    const config = {
+        method: 'post',
+        url: process.env.HARPERDB_URL,
+        headers: {
+            Authorization: `Basic ${process.env.HARPERDB_AUTH}`,
+            'Content-Type': 'application/json',
+        },
+        data: data,
+    };
+
+    axios(config)
+        .then((response) => {
+            res.send({ msg: 'Salary Deleted' });
+            console.log('Salary Deleted');
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+});
+
 
 const port = process.env.PORT || 5000;
 
--- mechanics-database/frontend/src/App.js
+++ mechanics-database/frontend/src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 const App = () => {
     useEffect(() => {
         const getAPI = () => {
-            // Change this endpoint to whatever local or online address you have
-            // Local PostgreSQL Database
+            // Harper DB endpoint
             const API = 'http://127.0.0.1:5000/';
 
             fetch(API)
@@ -24,35 +23,54 @@ const App = () => {
     const [apiData, setApiData] = useState([]);
     const [loading, setLoading] = useState(true);
     return (
-        // <Fragment>
+         <Fragment>
             <header>
                 <h1>Garage Band</h1>
             </header>
-        //     <div className="form-container">
-        //         <h2>Add Employee</h2>
-        //         <form method="POST" action="http://127.0.0.1:5000/add-employee">
-        //             <div>
-        //                 <label>Employee ID</label>
-        //                 <input type="text" name="employee_id" required />
-        //             </div>
-        //             <div>
-        //                 <label>Employee Name</label>
-        //                 <input type="text" name="employee_name" required />
-        //             </div>
-        //             <div>
-        //                 <label>Job</label>
-        //                 <input type="text" name="job_title" required />
-        //             </div>
-        //             <div>
-        //                 <label>Password</label>
-        //                 <input type="text" name="employee_password" required />
-        //             </div>
-        //             <div>
-        //                 <button type="submit">Add Employee</button>
-        //             </div>
-        //         </form>
-        //     </div>
-        // </Fragment>
+            <div className="form-container">
+                <h2>Add Employee</h2>
+                <form method="POST" action="http://127.0.0.1:5000/add-employee">
+                    <div>
+                        <label>Employee ID</label>
+                        <input type="text" name="employee_id" required />
+                    </div>
+                    <div>
+                        <label>Employee Name</label>
+                        <input type="text" name="employee_name" required />
+                    </div>
+                    <div>
+                        <label>Job</label>
+                        <input type="text" name="job_title" required />
+                    </div>
+                    <div>
+                        <label>Password</label>
+                        <input type="text" name="employee_password" required />
+                    </div>
+                    <div>
+                        <button type="submit">Add Employee</button>
+                    </div>
+                </form>
+              </div>
+              <h2 style = {{display: 'flex',  justifyContent:'center'}}> List of Employees</h2>
+              <section>
+                  {apiData.map((Employee) => {
+                      return (
+                          <div className="employee-container" key={String(Employee.employee_id)}>
+                              <h1>{Employee.employee_name}</h1>
+                              <p>
+                                  <strong>Employee ID:</strong> {Employee.employee_id}
+                              </p>
+                              <p>
+                                  <strong>Job:</strong> {Employee.job_title}
+                              </p>
+                              <p>
+                                  <strong>Password:</strong> {Employee.employee_password}
+                              </p>
+                          </div>
+                      );
+                  })}
+              </section>
+        </Fragment>
     );
 };
 

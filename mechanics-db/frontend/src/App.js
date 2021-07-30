16a20b56be21ff9b431fb8b7c3ff65bfccc250bc
pranavPka <57738646+pranavPka@users.noreply.github.com>
2021-07-30 00:21:01 -0500

    Add files via upload

---
+++ mechanics-db/frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router,Link, Route, Switch } from "react-router-dom";
+import {Home, Employees,Vehicles, Parts, Repair, Customers, AddEmployee, Navigation} from "./Components";
+import './App.css';
+
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path="/" exact component={() => <Home />} />
+          <Route path="/Employees" exact component={() => <Employees />} />
+          <Route path="/Vehicles" exact component={() => <Vehicles />} />
+          <Route path="/Parts" exact component={() => <Parts />} />
+          <Route path="/Repairs" exact component={() => <Repair />} />
+          <Route path="/Customers" exact component={() => <Customers />} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
---
+++ mechanics-db/frontend/src/Components/index.js
@@ -0,0 +1,8 @@
+export { default as Home } from "./Home";
+export { default as Employees } from "./Employees";
+export { default as Navigation } from "./Navigation";
+export { default as AddEmployee } from "./AddEmployee";
+export { default as Vehicles } from "./Vehicles";
+export { default as Parts } from "./Parts";
+export { default as Customers } from "./Customers";
+export { default as Repair } from "./Repair";

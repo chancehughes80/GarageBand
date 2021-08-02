2ae093f01d3dd54ce8be9324bf95ff895e315843
Divia Joseph <diviajoseph1@tamu.edu>
2021-08-02 16:40:27 -0500

    Merged working delete employee

--- mechanics-database/frontend/src/Components/Employees.js
+++ mechanics-database/frontend/src/Components/Employees.js
@@ -38,72 +38,79 @@ function Employees() {
         <header>
                   <h1>Employees</h1>
         </header>
-        <div className="form-container">
-                <h2>Add Employee</h2>
-                <form method="POST" action="http://127.0.0.1:5000/online/harperdb/employee/add-employee">
-                    <div>
-                        <label>Employee ID</label>
-                        <input type="text" name="employee_id" required />
-                    </div>
-                    <div>
-                        <label>Employee Name</label>
-                        <input type="text" name="employee_name" required />
-                    </div>
-                    <div>
-                        <label>Job</label>
-                        <input type="text" name="job_title" required />
-                    </div>
-                    <div>
-                        <label>Password</label>
-                        <input type="text" name="employee_password" required />
-                    </div>
-                    <div>
-                        <button type="submit">Add Employee</button>
-                    </div>
-                </form>
-              </div>
-        <main>
-            <section>
-                {apiData.map((Employee) => {
-                    return (
-                        <div className="employee-container" key={String(Employee.employee_id)}>
-                            <h1>{Employee.employee_name}</h1>
-                            <p>
-                                <strong>ID:</strong> {Employee.employee_id}
-                            </p>
-                            <p>
-                                <strong>Job:</strong> {Employee.job_title}
-                            </p>
-                          <p>
-                            <button onClick={() => removeData(Employee.employee_id)}>Delete</button>
-                          </p>
-
+        <div class="container">
+            <div class="row justify-items-center">
+                <div class="col-lg-4 top" >
+                    <form method="POST" action="http://127.0.0.1:5000/online/harperdb/employee/add-employee">
+                        <div>
+                            <label>Employee ID</label>
+                            <input type="text" name="employee_id" required />
                         </div>
-                    );
-                })}
-             </section>
-         </main>
-         <form method="POST" action="http://127.0.0.1:5000/online/harperdb/employee/update-employee">
-             <div>
-                 <label>Employee ID</label>
-                 <input type="text"  name="employee_id" required />
-             </div>
-             <div>
-                 <label>Employee Name</label>
-                 <input type="text"  name="employee_name" required />
-             </div>
-             <div>
-                 <label>Employee Password</label>
-                 <input type="text"  name="employee_password" required />
-             </div>
-             <div>
-                 <label>Job Title</label>
-                 <input type="text"  name="job_title" required />
-             </div>
-             <div>
-                 <button type="submit">Update Employee</button>
-             </div>
-         </form>
+                        <div>
+                            <label>Employee Name</label>
+                            <input type="text" name="employee_name" required />
+                        </div>
+                        <div>
+                            <label>Employee Password</label>
+                            <input type="text" name="employee_password" required />
+                        </div>
+                        <div>
+                            <label>Job Title</label>
+                            <input type="text" name="job_title" required />
+                        </div>
+                        <div>
+                            <button type="submit">Add Employee</button>
+                        </div>
+                    </form>
+
+
+                    <form method="PUT" action="http://127.0.0.1:5000/online/harperdb/employee/update-employee">
+                         <div>
+                             <label>Employee ID</label>
+                             <input type="text" name="employee_id" required />
+                         </div>
+                         <div>
+                             <label>Employee Name</label>
+                             <input type="text" name="employee_name" required />
+                         </div>
+                         <div>
+                             <label>Employee Password</label>
+                             <input type="text" name="employee_password" required />
+                         </div>
+                         <div>
+                             <label>Job Title</label>
+                             <input type="text" name="job_title" required />
+                         </div>
+                         <div>
+                             <button type="submit">Update Employee</button>
+                         </div>
+                     </form>
+                </div>
+
+                <div class="col-lg-8">
+                    <main>
+                        <section>
+                            {apiData.map((Employee) => {
+                                return (
+                                    <div className="employee-container" key={String(Employee.employee_id)}>
+                                        <h1>{Employee.employee_name}</h1>
+                                        <p>
+                                            <strong>ID:</strong> {Employee.employee_id}
+                                        </p>
+                                        <p>
+                                            <strong>Job:</strong> {Employee.job_title}
+                                        </p>
+                                        <p>
+                                            <button onClick={() => removeData(Employee.employee_id)}>Delete</button>
+                                        </p>
+                                    </div>
+                                );
+                            })}
+                         </section>
+                     </main>
+                </div>
+            </div>
+        </div>
       </Fragment>
     );
 }
--- mechanics-database/frontend/src/Components/Repairs.js
+++ mechanics-database/frontend/src/Components/Repairs.js
@@ -7,7 +7,7 @@ function Repair() {
         const getAPI = () => {
             // Change this endpoint to whatever local or online address you have
             // Local PostgreSQL Database
-            const API = 'http://127.0.0.1:5000/online/harperdb/repair';
+            const API = 'http://127.0.0.1:5000/online/harperdb/repairs';
 
             fetch(API)
                 .then((response) => {
@@ -26,30 +26,94 @@ function Repair() {
     const [loading, setLoading] = useState(true);
 
     return(
-        <main>
-            <a href="/AddRepairs">Add Repairs</a>
-            {loading === true ? (
-                <div>
-                    <h1>Loading...</h1>
+      <Fragment>
+        <header>
+                  <h1>Repairs</h1>
+        </header>
+        <div class="container">
+            <div class="row justify-items-center">
+                <div class="col-lg-4 top" >
+                    <form method="POST" action="http://127.0.0.1:5000/online/harperdb/employee/add-employee">
+                        <div>
+                            <label>Repair ID</label>
+                            <input type="text" name="employee_id" required />
+                        </div>
+                        <div>
+                            <label>Repair Description</label>
+                            <input type="text" name="employee_name" required />
+                        </div>
+                        <div>
+                            <label>Repair Time</label>
+                            <input type="text" name="employee_password" required />
+                        </div>
+                        <div>
+                            <label>Cost</label>
+                            <input type="text" name="job_title" required />
+                        </div>
+                        <div>
+                            <button type="submit">Add Repair</button>
+                        </div>
+                    </form>
+
+           
+                    <form method="PUT" action="http://127.0.0.1:5000/online/harperdb/employee/update-employee">
+                         <div>
+                             <label>Repair ID</label>
+                             <input type="text" name="employee_id" required />
+                         </div>
+                         <div>
+                             <label>Repair Description</label>
+                             <input type="text" name="employee_name" required />
+                         </div>
+                         <div>
+                             <label>Repair Time</label>
+                             <input type="text" name="employee_password" required />
+                         </div>
+                         <div>
+                             <label>Cost </label>
+                             <input type="text" name="job_title" required />
+                         </div>
+                         <div>
+                             <button type="submit">Update Repair</button>
+                         </div>
+                     </form>
+
+                     <form method="DELETE" action="http://127.0.0.1:5000/online/harperdb/employee/delete-employee">
+                         <div>
+                             <label>Employee ID</label>
+                             <input type="text" name="employee_id" required />
+                         </div>
+                         <div>
+                             <button type="submit">Delete Employee</button>
+                         </div>
+                    </form>
                 </div>
-            ) : (
-                <section>
-                    {apiData.map((repair) => {
-                        return (
-                            <div className="repair-container" key={String(repair.repair_id)}>
-                                <h1>{repair.repair_description}</h1>
-                                <p>
-                                    <strong>ID:</strong> {repair.repair_id}
-                                </p>
-                                <p>
-                                    <strong>Job:</strong> {repair.repair_cost}
-                                </p>
-                            </div>
-                        );
-                    })}
-                 </section>
-            )}
-         </main>
+
+                <div class="col-lg-8">
+                    <main>
+                        <section>
+                            {apiData.map((Repairs) => {
+                                return (
+                                    <div className="employee-container" key={String(Repairs.employee_id)}>
+                                        <h1>{Repairs.repair_description}</h1>
+                                        <p>
+                                            <strong>ID:</strong> {Repairs.repair_id}
+                                        </p>
+                                        <p>
+                                            <strong>Cost:</strong> {Repairs.repair_cost}
+                                        </p>
+                                        <p>
+                                            <strong>Total Time:</strong> {Repairs.estimated_time_for_repair} min
+                                        </p>
+                                    </div>
+                                );
+                            })}
+                         </section>
+                     </main>
+                </div>
+            </div>
+        </div>
+      </Fragment>
     );
 }
 

0e6c1f43a2d26951163c3f6d92edf62e5a8f0c61
chancehughes80 <61440396+chancehughes80@users.noreply.github.com>
2021-08-04 14:05:08 -0500

    Trying to fix VehicleRepair

---
+++ mechanics-database/frontend/src/Components/VehicleRepair.js
@@ -0,0 +1,222 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import MaterialTable from 'material-table';
+import axios from 'axios';
+import './App.css';
+
+
+function VehicleRepair() {
+
+    var columns = [
+        { title: 'VIN', field: 'VIN', editable: 'onAdd'},
+        { title: 'Repair ID', field: 'repair_id'},
+        { title: 'Status', field: 'repair_status'},
+        { title: 'Actual Time for Repair', field: 'actual_time'}
+      ]
+
+    const [status, setStatus] = useState(null);
+    const[VIN, setVIN] = useState('');
+    const[repair_id, setID] = useState('');
+    const[repair_status, setRepairStatus] = useState('');
+    const[actual_time, setTime] = useState('');
+
+    const [apiData, setApiData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState(null);
+    const [isError, setIsError] = useState(false);
+    const [errorMessages, setErrorMessages] = useState([])
+
+    useEffect(() => {
+        const getAPI = () => {
+            // Change this endpoint to whatever local or online address you have
+            // Local PostgreSQL Database
+            const API = 'http://127.0.0.1:5000/online/harperdb/repair-vehicle';
+
+            fetch(API)
+                .then((response) => {
+                    console.log(response);
+                    return response.json();
+                })
+                .then((data) => {
+                    console.log(data);
+                    setLoading(false);
+                    setApiData(data);
+                });
+        };
+        getAPI();
+    }, []);
+
+    const handleSubmit = () => {
+        setLoading(true);
+        setIsError(false);
+        const data = {
+          VIN: VIN,
+          repair_id: repair_id,
+          repair_status: repair_status,
+          actual_time: actual_time,
+        }
+        axios.put('http://127.0.0.1:5000/online/harperdb/repair-vehicle/update-repair-vehicle', data)
+          .then(res => {
+            setData(res.data);
+            setVIN('');
+            setID('');
+            setRepairStatus('');
+            setTime('');
+            setLoading(false);
+          }).catch(err => {
+            setLoading(false);
+            setIsError(true);
+          });
+        }
+
+    const handleRowAdd = (newData, resolve) => {
+        //validation
+        let errorList = []
+        if(newData.VIN === undefined){
+            errorList.push("Please enter VIN: ")
+        }
+        if(newData.repair_id === undefined){
+            errorList.push("Please enter Repair ID")
+        }
+        if(newData.repair_status === undefined){
+            errorList.push("Please enter a Status")
+        }
+        if(newData.actual_time === undefined){
+            errorList.push("Please enter an Actual Repair Time")
+        }
+        const url = 'http://127.0.0.1:5000/online/harperdb/repair-vehicle/add-repair-vehicle';
+        if(errorList.length < 1){ //no error
+            axios.post(url, newData)
+            .then(res => {
+            let dataToAdd = [...data];
+            dataToAdd.push(newData);
+            setData(dataToAdd);
+            resolve()
+            setErrorMessages([])
+            setIsError(false)
+            })
+            .catch(error => {
+            setErrorMessages(["Cannot add data. Server error!"])
+            setIsError(true)
+            resolve()
+            })
+        }else{
+            setErrorMessages(errorList)
+            setIsError(true)
+            resolve()
+        }
+        window.location.reload(false);
+
+    }
+
+    const handleRowDelete = (oldData, resolve) =>{
+        const url = 'http://127.0.0.1:5000/online/harperdb/repair-vehicle/delete-repair-vehicle/' + oldData.VIN;
+        axios.delete(url)
+          .then(res => {
+            const dataDelete = [...data];
+            const index = oldData.tableData.VIN;
+            dataDelete.splice(index, 1);
+            setData([...dataDelete]);
+            resolve()
+          })
+          .catch(error => {
+             setErrorMessages(["Delete failed! Server error"])
+             setIsError(true)
+             resolve()
+           })
+           window.location.reload(false);
+      }
+    
+    const handleRowUpdate = (newData, oldData, resolve) => {
+        //validation
+        let errorList = []
+        if(newData.VIN === undefined){
+            errorList.push("Please enter VIN: ")
+        }
+        if(newData.repair_id === undefined){
+            errorList.push("Please enter Repair ID")
+        }
+        if(newData.repair_status === undefined){
+            errorList.push("Please enter a Status")
+        }
+        if(newData.actual_time === undefined){
+            errorList.push("Please enter an Actual Repair Time")
+        }
+        if(errorList.length < 1){
+            axios.put("http://127.0.0.1:5000/online/harperdb/repair-vehicle/update-repair-vehicle", newData)
+            .then(res => {
+                const dataUpdate = [...data];
+                const index = oldData.tableData.VIN;
+                dataUpdate[index] = newData;
+                setData([...dataUpdate]);
+                resolve()
+                setIsError(false)
+                setErrorMessages([])
+            })
+            .catch(error => {
+                setErrorMessages(["Update failed!"])
+                setIsError(true)
+                resolve()
+            })
+        }else{
+            setErrorMessages(errorList)
+            setIsError(true)
+            resolve()
+        }
+        window.location.reload(false);
+    }
+
+    return(
+        <Fragment>
+          <header>
+                    <h1>Vehicle Repair</h1>
+          </header>
+          <div class="container">
+  
+                      <main class="spacer">
+  
+                          <MaterialTable
+                              title="VehicleRepair"
+                              columns={columns}
+                              data={apiData}
+                              style={{
+                                  border: "3px solid #744F28",
+                                  maxWidth: "1450px",
+                                  overflow: "scroll",
+                                  background: "#eaeaea",
+                                  color: "#500000",
+                              }}
+                              options={{
+                                 headerStyle: {
+                                      background: "#d1d1d1",
+                                      color: '#500000',
+                                  },
+                                  cellStyle: {
+                                      color: '#500000',
+                                  }
+                              }}
+                              editable={{
+                                  onRowAdd: (newData) =>
+                                      new Promise((resolve) => {
+                                          handleRowAdd(newData, resolve)
+                                      }),
+                                  onRowUpdate: (newData, oldData) =>
+                                      new Promise((resolve) => {
+                                          handleRowUpdate(newData, oldData, resolve);
+                                      }),
+                                  onRowDelete: oldData =>
+                                      new Promise((resolve) => {
+                                          handleRowDelete(oldData, resolve)
+                                    }),
+                              }}
+                          />
+                      </main>
+  
+  
+          </div>
+        </Fragment>
+      );
+
+    
+}
+
+export default VehicleRepair;
---
+++ mechanics-database/frontend/src/Components/VehicleType.js
@@ -0,0 +1,202 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import MaterialTable from 'material-table';
+import axios from 'axios';
+import './App.css';
+
+
+function VehicleType() {
+
+    var columns = [
+        { title: 'Model', field: 'model', editable: 'onAdd'},
+        { title: 'Make', field: 'make'},
+      ]
+
+    const [status, setStatus] = useState(null);
+    const[model, setModel] = useState('');
+    const[make, setMake] = useState('');
+
+    const [apiData, setApiData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState(null);
+    const [isError, setIsError] = useState(false);
+    const [errorMessages, setErrorMessages] = useState([])
+
+    useEffect(() => {
+        const getAPI = () => {
+            // Change this endpoint to whatever local or online address you have
+            // Local PostgreSQL Database
+            const API = 'http://127.0.0.1:5000/online/harperdb/vehicle-type';
+
+            fetch(API)
+                .then((response) => {
+                    console.log(response);
+                    return response.json();
+                })
+                .then((data) => {
+                    console.log(data);
+                    setLoading(false);
+                    setApiData(data);
+                });
+        };
+        getAPI();
+    }, []);
+
+    const handleSubmit = () => {
+        setLoading(true);
+        setIsError(false);
+        const data = {
+          model: model,
+          make: make,
+        }
+        axios.put('http://127.0.0.1:5000/online/harperdb/vehicle-type/update-vehicle-type', data)
+          .then(res => {
+            setData(res.data);
+            setModel('');
+            setMake('');
+            setLoading(false);
+          }).catch(err => {
+            setLoading(false);
+            setIsError(true);
+          });
+        }
+
+    const handleRowAdd = (newData, resolve) => {
+        //validation
+        let errorList = []
+        if(newData.model === undefined){
+            errorList.push("Please enter Model: ")
+        }
+        if(newData.make === undefined){
+            errorList.push("Please enter Make")
+        }
+        const url = 'http://127.0.0.1:5000/online/harperdb/vehicle-type/add-vehicle-type';
+        if(errorList.length < 1){ //no error
+            axios.post(url, newData)
+            .then(res => {
+            let dataToAdd = [...data];
+            dataToAdd.push(newData);
+            setData(dataToAdd);
+            resolve()
+            setErrorMessages([])
+            setIsError(false)
+            })
+            .catch(error => {
+            setErrorMessages(["Cannot add data. Server error!"])
+            setIsError(true)
+            resolve()
+            })
+        }else{
+            setErrorMessages(errorList)
+            setIsError(true)
+            resolve()
+        }
+        window.location.reload(false);
+
+    }
+
+    const handleRowDelete = (oldData, resolve) =>{
+        const url = 'http://127.0.0.1:5000/online/harperdb/vehicle-type/delete-vehicle-type/' + oldData.model;
+        axios.delete(url)
+          .then(res => {
+            const dataDelete = [...data];
+            const index = oldData.tableData.model;
+            dataDelete.splice(index, 1);
+            setData([...dataDelete]);
+            resolve()
+          })
+          .catch(error => {
+             setErrorMessages(["Delete failed! Server error"])
+             setIsError(true)
+             resolve()
+           })
+           window.location.reload(false);
+      }
+    
+    const handleRowUpdate = (newData, oldData, resolve) => {
+        //validation
+        let errorList = []
+        if(newData.VIN === undefined){
+            errorList.push("Please enter Model: ")
+        }
+        if(newData.repair_id === undefined){
+            errorList.push("Please enter Make")
+        }
+        if(errorList.length < 1){
+            axios.put("http://127.0.0.1:5000/online/harperdb/vehicle-type/update-vehicle-type", newData)
+            .then(res => {
+                const dataUpdate = [...data];
+                const index = oldData.tableData.VIN;
+                dataUpdate[index] = newData;
+                setData([...dataUpdate]);
+                resolve()
+                setIsError(false)
+                setErrorMessages([])
+            })
+            .catch(error => {
+                setErrorMessages(["Update failed!"])
+                setIsError(true)
+                resolve()
+            })
+        }else{
+            setErrorMessages(errorList)
+            setIsError(true)
+            resolve()
+        }
+        window.location.reload(false);
+    }
+
+    return(
+        <Fragment>
+          <header>
+                    <h1>Vehicle Type</h1>
+          </header>
+          <div class="container">
+  
+                      <main class="spacer">
+  
+                          <MaterialTable
+                              title="VehicleType"
+                              columns={columns}
+                              data={apiData}
+                              style={{
+                                  border: "3px solid #744F28",
+                                  maxWidth: "1450px",
+                                  overflow: "scroll",
+                                  background: "#eaeaea",
+                                  color: "#500000",
+                              }}
+                              options={{
+                                 headerStyle: {
+                                      background: "#d1d1d1",
+                                      color: '#500000',
+                                  },
+                                  cellStyle: {
+                                      color: '#500000',
+                                  }
+                              }}
+                              editable={{
+                                  onRowAdd: (newData) =>
+                                      new Promise((resolve) => {
+                                          handleRowAdd(newData, resolve)
+                                      }),
+                                  onRowUpdate: (newData, oldData) =>
+                                      new Promise((resolve) => {
+                                          handleRowUpdate(newData, oldData, resolve);
+                                      }),
+                                  onRowDelete: oldData =>
+                                      new Promise((resolve) => {
+                                          handleRowDelete(oldData, resolve)
+                                    }),
+                              }}
+                          />
+                      </main>
+  
+  
+          </div>
+        </Fragment>
+      );
+
+    
+}
+
+export default VehicleType;

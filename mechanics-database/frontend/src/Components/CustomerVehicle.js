a316c5e488e598f398d933e417200e39baf4e305
Divia Joseph <diviajoseph1@tamu.edu>
2021-08-04 15:13:16 -0500

    Working Manage your Vehicle

--- mechanics-database/frontend/src/Components/CustomerVehicle.js
+++ mechanics-database/frontend/src/Components/CustomerVehicle.js
@@ -11,7 +11,7 @@ function CustomerVehicle() {
     { title: 'Color', field: 'color'},
     { title: 'Plate', field: 'plate'},
     { title: 'Year', field: 'vehicle_year'},
-    { title: 'Customer ID', field: 'customer_id'}
+    { title: 'Customer ID', field: 'customer_id'},
 
   ]
   const [status, setStatus] = useState(null);
@@ -20,12 +20,12 @@ function CustomerVehicle() {
   const [data, setData] = useState(null);
   const [isError, setIsError] = useState(false);
   const [errorMessages, setErrorMessages] = useState([])
-  
+
   useEffect(() => {
       const getAPI = () => {
           // Change this endpoint to whatever local or online address you have
           // Local PostgreSQL Database
-          const API = 'http://127.0.0.1:5000/online/harperdb/vehicle/451';
+          const API = 'http://127.0.0.1:5000/online/harperdb/vehicle/455';
 
           fetch(API)
               .then((response) => {
@@ -42,91 +42,112 @@ function CustomerVehicle() {
   }, []);
 
   const handleRowAdd = (newData, resolve) => {
-    //validation
-    let errorList = []
-    if(newData.VIN === undefined){
-        errorList.push("Please enter VIN: ")
-    }
-    if(newData.model === undefined){
-        errorList.push("Please enter Model")
-    }
-    if(newData.year === undefined){
-        errorList.push("Please enter a Year")
-    }
-    if(newData.color === undefined){
-        errorList.push("Please enter a Color")
-    }
-    if(newData.plate === undefined){
-        errorList.push("Please enter a Plate ID")
-    }
-    if(newData.customer_id === undefined){
-        errorList.push("Please enter an Customer ID")
-    }
-    const url = 'http://127.0.0.1:5000/online/harperdb/vehicle/add-vehicle';
-    if(errorList.length < 1){ //no error
-        axios.post(url, newData)
+      //validation
+      let errorList = []
+      if(newData.VIN === undefined){
+          errorList.push("Please enter VIN: ")
+      }
+      if(newData.model === undefined){
+          errorList.push("Please enter Model")
+      }
+      if(newData.vehicle_year === undefined){
+          errorList.push("Please enter a Year")
+      }
+      if(newData.color === undefined){
+          errorList.push("Please enter a Color")
+      }
+      if(newData.plate === undefined){
+          errorList.push("Please enter a Plate ID")
+      }
+      if(newData.customer_id === undefined){
+          errorList.push("Please enter an Customer ID")
+      }
+      const url = 'http://127.0.0.1:5000/online/harperdb/vehicle/add-vehicle';
+      if(errorList.length < 1){ //no error
+          axios.post(url, newData)
+          .then(res => {
+          let dataToAdd = [...data];
+          dataToAdd.push(newData);
+          setData(dataToAdd);
+          resolve()
+          setErrorMessages([])
+          setIsError(false)
+          })
+          .catch(error => {
+          setErrorMessages(["Cannot add data. Server error!"])
+          setIsError(true)
+          resolve()
+          })
+      }else{
+          setErrorMessages(errorList)
+          setIsError(true)
+          resolve()
+      }
+      window.location.reload(false);
+
+  }
+
+  const handleRowDelete = (oldData, resolve) =>{
+      const url = 'http://127.0.0.1:5000/online/harperdb/vehicle/delete-vehicle/' + oldData.VIN;
+      axios.delete(url)
         .then(res => {
-        let dataToAdd = [...data];
-        dataToAdd.push(newData);
-        setData(dataToAdd);
-        resolve()
-        setErrorMessages([])
-        setIsError(false)
+          const dataDelete = [...data];
+          const index = oldData.tableData.VIN;
+          dataDelete.splice(index, 1);
+          setData([...dataDelete]);
+          resolve()
         })
         .catch(error => {
-        setErrorMessages(["Cannot add data. Server error!"])
-        setIsError(true)
-        resolve()
-        })
-    }else{
-        setErrorMessages(errorList)
-        setIsError(true)
-        resolve()
+           setErrorMessages(["Delete failed! Server error"])
+           setIsError(true)
+           resolve()
+         })
+         window.location.reload(false);
     }
-    window.location.reload(false);
-
-}
 
   const handleRowUpdate = (newData, oldData, resolve) => {
-    //validation
-    let errorList = []
-    if(newData.VIN === undefined){
-      errorList.push("Please enter VIN: ")
-    }
-    if(newData.model === undefined){
-      errorList.push("Please enter model:")
-    }
-    if(newData.color === undefined){
-      errorList.push("Please enter a color:")
-    }
-    if(newData.plate === undefined){
-      errorList.push("Please enter a plate number:")
-    }
-    if(newData.year === undefined){
-      errorList.push("Please enter a year:")
-    }
-    if(errorList.length < 1){
-        axios.put("http://127.0.0.1:5000/online/harperdb/customer/update-customer", newData)
-        .then(res => {
-            const dataUpdate = [...data];
-            const index = oldData.tableData.customer_id;
-            dataUpdate[index] = newData;
-            setData([...dataUpdate]);
-            resolve()
-            setIsError(false)
-            setErrorMessages([])
-        })
-        .catch(error => {
-            setErrorMessages(["Update failed!"])
-            setIsError(true)
-            resolve()
-        })
-    }else{
-        setErrorMessages(errorList)
-        setIsError(true)
-        resolve()
-    }
-    window.location.reload(false);
+      //validation
+      let errorList = []
+      if(newData.VIN === undefined){
+          errorList.push("Please enter a VIN: ")
+      }
+      if(newData.model === undefined){
+          errorList.push("Please enter Model")
+      }
+      if(newData.vehicle_year === undefined){
+          errorList.push("Please enter a Year")
+      }
+      if(newData.color === undefined){
+          errorList.push("Please enter a Color")
+      }
+      if(newData.plate === undefined){
+          errorList.push("Please enter a Plate ID")
+      }
+      if(newData.customer_id === undefined){
+          errorList.push("Please enter a Customer ID")
+      }
+      if(errorList.length < 1){
+          axios.put("http://127.0.0.1:5000/online/harperdb/vehicle/update-vehicle", newData)
+          .then(res => {
+              const dataUpdate = [...data];
+              const index = oldData.tableData.VIN;
+              dataUpdate[index] = newData;
+              setData([...dataUpdate]);
+              resolve()
+              setIsError(false)
+              setErrorMessages([])
+          })
+          .catch(error => {
+              setErrorMessages(["Update failed!"])
+              setIsError(true)
+              resolve()
+          })
+      }else{
+          setErrorMessages(errorList)
+          setIsError(true)
+          resolve()
+      }
+      window.location.reload(false);
 }
 
 
@@ -135,7 +156,7 @@ function CustomerVehicle() {
       <header>
                 <h1>Manage Your Vehicle Information</h1>
       </header>
-     
+
       <div class="container">
 
 <main class="spacer">
@@ -169,7 +190,7 @@ function CustomerVehicle() {
                 new Promise((resolve) => {
                     handleRowUpdate(newData, oldData, resolve);
                 })
-                                
+
         }}
     />
 </main>
